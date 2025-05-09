70789124b11a6c4c82ad7c6bbed50100bdd7620a
Alexander Taran <alex.y.taran@gmail.com>
2025-05-10 01:51:28 +0200

    Fix version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "net.alextaran.sms2tg"
         minSdk = 33
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
